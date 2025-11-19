e46eb9c83d58cfe1d6fad49d1d2d8823cbf662de
GoJunHyeob <jhk010529@gmail.com>
2025-11-19 14:59:35 +0900

    플레이어점프및좌우이동

---
+++ Assets/PlayerMoveScripts/PlayerMoveController.cs
@@ -0,0 +1,45 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMoveController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed = 7f;
+
+    [Header("Jump")]
+    public float jumpForce = 12f;
+    public Transform groundCheck;
+    public float checkRadius = 0.15f;
+    public LayerMask groundLayer;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.freezeRotation = true; // 충돌 시 회전 방지
+    }
+
+    void Update()
+    {
+       
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
+
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocity = new Vector2(moveX * moveSpeed, rb.linearVelocity.y);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(groundCheck.position, checkRadius);
+    }
+}
