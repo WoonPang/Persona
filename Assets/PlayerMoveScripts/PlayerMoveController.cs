344d7a510984d18528b0800661db1b3878068686
GoJunHyeob <jhk010529@gmail.com>
2025-11-30 19:03:37 +0900

    feat:애니매이션 적용

--- Assets/PlayerMoveScripts/PlayerMoveController.cs
+++ Assets/PlayerMoveScripts/PlayerMoveController.cs
@@ -3,19 +3,26 @@ using UnityEngine;
 
 public class PlayerMoveController : MonoBehaviour
 {
+    private SpriteRenderer sprite;
+    private Animator anim;
+    private Rigidbody2D rb;
+
     [Header("Movement")]
     public float moveSpeed = 7f;
+    private float moveX;
+    public int lastDirection;
 
     [Header("Jump")]
     public float jumpForce = 12f;
     public Transform groundCheck;
-    public float checkRadius = 0.15f;
+    public float checkRadius = 0.35f;
     public LayerMask groundLayer;
-    private Rigidbody2D rb;
     private bool isGrounded;
 
     void Start()
     {
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
         rb.freezeRotation = true; // 충돌 시 회전 방지
     }
@@ -30,11 +37,26 @@ public class PlayerMoveController : MonoBehaviour
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
     }
-
     void FixedUpdate()
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
+        moveX = Input.GetAxisRaw("Horizontal");
         rb.linearVelocity = new Vector2(moveX * moveSpeed, rb.linearVelocity.y);
+    }
+
+    void LateUpdate()
+    {
+        SetDirec();
+
+        HandleJumpAnimations();
+
+        if (moveX != 0)
+        {
+            anim.SetBool("isMove", true);
+        }
+        else
+        {
+            anim.SetBool("isMove", false);
+        }
     }
 
     private void OnDrawGizmos()
@@ -42,4 +64,24 @@ public class PlayerMoveController : MonoBehaviour
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(groundCheck.position, checkRadius);
     }
+
+    void SetDirec()
+    {
+        if (moveX > 0) lastDirection = 1;
+        else if (moveX < 0) lastDirection = -1;
+
+        if (lastDirection == 1) sprite.flipX = false;
+        else sprite.flipX = true;
+    }
+
+    void HandleJumpAnimations()
+    {
+        // Animator에 isGrounded 값을 전달
+        anim.SetBool("isGround", isGrounded);
+
+        // y축 속도 값 전달
+        float yVelocity = rb.linearVelocity.y;
+        anim.SetFloat("yVelocity", yVelocity);
+    }
+
 }
